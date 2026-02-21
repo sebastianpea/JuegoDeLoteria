a24c4ef5f2d9b3381717d1c642cc7d3d55ae4e6d
seb <snapsync29@gmail.com>
2026-02-21 14:19:06 -0600

    Se agregaron y se modificaron los nombres de las cartas, para tener una mayor facilidad al abrir el projecto, no tener que descargar fotos externas para que funcione correctamente, las imagenes son de "stand-by" en lo que se editan para tener el mismo tamaño. Y se modifico el nombre de las mismas para coinicdir con los recursos.

--- JuegoDeLoteria/Juego/Carta.cs
+++ JuegoDeLoteria/Juego/Carta.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+using JuegoDeLoteria.Properties;
 
 namespace JuegoDeLoteria.Juego
 {
@@ -17,6 +14,10 @@ namespace JuegoDeLoteria.Juego
             Id = id;
             Nombre = nombre;
             Imagen = imagen;
+        }
+        public Image ObtenerImagen()
+        {
+            return (Image)Resources.ResourceManager.GetObject(Imagen)!;
         }
 
 

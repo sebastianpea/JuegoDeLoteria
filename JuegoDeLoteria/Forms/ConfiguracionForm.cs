39aefa9145ee42dd222533475667591ee52dce1f
seb <snapsync29@gmail.com>
2026-02-22 15:01:46 -0600

    Se agregaron todos los forms necesarios para poder hacer el juego interactivo, configuracion(volumen, musica, etc), Dialogo (Cuando inicias el juego),  Juego, Lobby, PostJuego, y Unirse

---
+++ JuegoDeLoteria/Forms/ConfiguracionForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JuegoDeLoteria.Forms
+{
+    public partial class ConfiguracionForm : Form
+    {
+        public ConfiguracionForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
